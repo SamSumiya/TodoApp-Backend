7d02f0fc1117a855eb40f04fc978e9b08e4cfda4
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-09-16 23:01:20 -0400

    initial

---
+++ __tests__/app.test.js
@@ -0,0 +1,14 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+describe('demo routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
