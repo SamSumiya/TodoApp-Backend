2fd7d7b0e95d921083113d1561deca60733eee6a
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-09-21 09:22:05 -0400

    passed the first get all test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Todo from '../lib/models/Todo';
 
 describe('demo routes', () => {
   beforeEach(() => {
@@ -10,5 +11,29 @@ describe('demo routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('get a list of todos', async () => {
+    const todo1 = {
+      description: 'This is my first todo for tomorrow',
+      isCompleted: false,
+    };
+    const todo2 = {
+      description: 'This is my first finished todo for today',
+      isCompleted: true,
+    };
+    await Todo.insert(todo1.description, todo1.isCompleted);
+    await Todo.insert(todo2.description, todo2.isCompleted);
+    const todos = await request(app).get('/api/v1/todos');
+    expect(todos.body).toEqual([
+      {
+        id: '1',
+        ...todo1,
+      },
+      {
+        id: '2',
+        ...todo2,
+      },
+    ]);
   });
 });
